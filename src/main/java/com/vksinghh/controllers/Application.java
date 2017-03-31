b9db8396becad04a524f001eceff2a9d116041d2
vksinghh25 <vipan.k.singh@gmail.com>
2017-03-31 16:23:32 +0530

    Multiple Threads

--- src/main/java/com/vksinghh/controllers/Application.java
+++ src/main/java/com/vksinghh/controllers/Application.java
@@ -2,7 +2,11 @@ package com.vksinghh.controllers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 /**
  * User: vksinghh
@@ -14,5 +18,10 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean(destroyMethod = "shutdown")
+    public Executor taskScheduler() {
+        return Executors.newScheduledThreadPool(5);
     }
 }
--- src/main/java/com/vksinghh/controllers/ScheduledTasks.java
+++ src/main/java/com/vksinghh/controllers/ScheduledTasks.java
@@ -32,25 +32,50 @@ public class ScheduledTasks {
         String newMessage = "This is a random message " + (long) (Math.random() * 1000);
         SendMessageRequest sendMessageRequest = new SendMessageRequest(getQueueUrlResult.getQueueUrl(), newMessage);
 
+        log.info("******* Sending ********");
         log.info("Sending msg to queue : " + newMessage);
         log.info("queueUrl : " + getQueueUrlResult.getQueueUrl());
         log.info("Thread Name : " + Thread.currentThread().getName());
-	sqsClient.sendMessage(sendMessageRequest);
+        log.info("*********************");
+
+        sqsClient.sendMessage(sendMessageRequest);
     }
 
-    @Scheduled(fixedRate = 5000)
+    @Scheduled(fixedRate = 3000)
     public void readFromQueue() {
         AmazonSQS sqsClient = AmazonSQSClientBuilder.standard().withCredentials(new ProfileCredentialsProvider()).withRegion(Regions.US_EAST_1).build();
         GetQueueUrlResult getQueueUrlResult = sqsClient.getQueueUrl("vksinghhqueue");
 
         ReceiveMessageRequest receiveMessageRequest = new ReceiveMessageRequest(getQueueUrlResult.getQueueUrl()).withMaxNumberOfMessages(5);
         List<Message> messages = sqsClient.receiveMessage(receiveMessageRequest).getMessages();
+
+        log.info("******* Receiving ********");
         for (Message message : messages) {
             log.info("  Message");
             log.info("    MessageId:     " + message.getMessageId());
             log.info("    Body:          " + message.getBody());
         }
-	log.info("Thread Name : " + Thread.currentThread().getName());
+        log.info("Thread Name : " + Thread.currentThread().getName());
+        log.info("*********************");
+
+    }
+
+    @Scheduled(fixedRate = 4000)
+    public void readFromQueueAgain() {
+        AmazonSQS sqsClient = AmazonSQSClientBuilder.standard().withCredentials(new ProfileCredentialsProvider()).withRegion(Regions.US_EAST_1).build();
+        GetQueueUrlResult getQueueUrlResult = sqsClient.getQueueUrl("vksinghhqueue");
+
+        ReceiveMessageRequest receiveMessageRequest = new ReceiveMessageRequest(getQueueUrlResult.getQueueUrl()).withMaxNumberOfMessages(5);
+        List<Message> messages = sqsClient.receiveMessage(receiveMessageRequest).getMessages();
+
+        log.info("******* Receiving ********");
+        for (Message message : messages) {
+            log.info("  Message");
+            log.info("    MessageId:     " + message.getMessageId());
+            log.info("    Body:          " + message.getBody());
+        }
+        log.info("Thread Name : " + Thread.currentThread().getName());
+        log.info("*********************");
 
     }
 }
