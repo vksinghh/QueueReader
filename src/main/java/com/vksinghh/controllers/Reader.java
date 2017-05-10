50add57b9c06a08179437520efee5e941e5f53c7
vipkumar <vipkumar@expedia.com>
2017-05-11 00:41:31 +0530

    Delete

--- src/main/java/com/vksinghh/controllers/Reader.java
+++ src/main/java/com/vksinghh/controllers/Reader.java
@@ -4,6 +4,7 @@ import com.amazonaws.auth.profile.ProfileCredentialsProvider;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.sqs.AmazonSQS;
 import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
+import com.amazonaws.services.sqs.model.DeleteMessageRequest;
 import com.amazonaws.services.sqs.model.GetQueueUrlResult;
 import com.amazonaws.services.sqs.model.Message;
 import com.amazonaws.services.sqs.model.ReceiveMessageRequest;
@@ -32,6 +33,11 @@ public class Reader {
 
         log.info("Start Receiving messages through thread : " + Thread.currentThread().getName());
         List<Message> messages = sqsClient.receiveMessage(receiveMessageRequest).getMessages();
+        for (Message message : messages) {
+            System.out.println("********" + message.getMessageId() + "*********");
+            DeleteMessageRequest deleteMessageRequest = new DeleteMessageRequest(getQueueUrlResult.getQueueUrl(), message.getReceiptHandle());
+            sqsClient.deleteMessage(deleteMessageRequest);
+        }
         log.info("End Receiving messages through thread : " + Thread.currentThread().getName());
     }
 }
